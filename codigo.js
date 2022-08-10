350d0be7c22ab77d00fa53ffb8e0e2593718abba
Rocio Romano <rociobromano5@gmail.com>
2022-08-09 22:02:16 -0300

    Modificaciones

--- codigo.js
+++ codigo.js
@@ -9,6 +9,7 @@ alert( "Bienvenido/a a nuestra tienda " + usuario);
 
 
 //carrito
+
 const libreria=[
     {
         articulo:"Resaltador Trabi",
@@ -16,7 +17,7 @@ const libreria=[
         precio: 70
     },
     {
-        articulo:"Resaltador Filgo TextMArket",
+        articulo:"Resaltador Filgo TextMarket",
         seccion:"Marcadores",
         precio: 90
     },
@@ -100,7 +101,7 @@ const Juguetes=[
         precio: 5000
     },
     {
-        articulo:"Tocsdor Gigante Fiorella",
+        articulo:"Tocador Gigante Fiorella",
         seccion:"Dia del niño",
         precio: 4800
     },
@@ -118,40 +119,58 @@ const Juguetes=[
     }
 
 const Carrito=libreria.concat(Juguetes);
+alert(Carrito);
 
-let buscarArticuloLib=prompt("Ingresa el articulo de libreria que deseas buscar, si no es de libreria ingresa un punto ");
-for (const libr of libreria){
+let buscarArticuloLib=prompt("Ingresa el articulo de libreria que deseas buscar,\n Resaltador Trabi \nResaltador Filgo TextMArket \nResaltador Trabi texting x6 \nResaltador Filgo Fine Ligther x 1 \nMarcadores Duo brush \nMarcadores Filgo metalizados x4 \nLapices de colores Filgox 12 \nLapiz filgo pastel color negro x1 \nCuaderno a4 Escolar \nCuaderno a5 Inkdrop \nCuaderno ABC x100hjs \nCuaderno A4 Triunfante 1 2 3 x 100hs \nAnotador tapa pastel \nCuaderno chico tapa dura \n \nsi no es de libreria ingresa un punto ");
+function BusquedaLib(){
+    for (const libr of libreria){
     if (libr.articulo == buscarArticuloLib){
      let ArticuloBuscado=libreria.indexOf(libr);
 
-     alert("El articulo que esta buscando se encuentra en el orden "+ ArticuloBuscado + "de Libreria");
+     console.log("El articulo que esta buscando se encuentra en el orden "+ ArticuloBuscado + " de Libreria");
+
+    }
+    else{
+        console.log("Este articulo no es valido o no se encuentra disponible")
+        break;
+    }
+    
+}
+}
+BusquedaLib();
+function BusquedaJug(){
+
+    let buscarArticuloJug=prompt("Ingresa el articulo de Jugueteria que deseas buscar: \n  Camion mega San Remo \nCocinita Gigante Fiorella \nTocador Gigante Fiorella \nExcavadora San Remo ");
+    for (const jug of Juguetes){
+        if (jug.articulo == buscarArticuloJug){
+            let ArticuloBuscado=Juguetes.indexOf(jug);
+            
+            alert("El articulo que esta buscando se encuentra en el orden "+ ArticuloBuscado + " de Jugueteria");
+            
+        }
+        else{
+            alert("Este articulo no es valido o no se encuentra disponible")
+        }
 
     }
 }
- let buscarArticuloJug=prompt("Ingresa el articulo de Jugueteria que deseas buscar");
-for (const jug of Juguetes){
-    if (jug.articulo == buscarArticuloJug){
-     let ArticuloBuscado=Juguetes.indexOf(jug);
+BusquedaJug();
 
-     alert("El articulo que esta buscando se encuentra en el orden "+ ArticuloBuscado + " de Jugueteria");
-
-    }
-}
- libreria.forEach((artLibreria)=>console.log(artLibreria.articulo));
+    libreria.forEach((artLibreria)=>console.log(artLibreria.articulo));
+    Juguetes.forEach((artJuguetes)=>console.log(artJuguetes.articulo));
 
  //FIND
  let ingreseSuBusqueda=prompt("Que articulo desea buscar?")
- let busqueda = libreria.find((el)=>el.articulo === ingreseSuBusqueda);
+ let busqueda = libreria.find((artLibreria)=>artLibreria.articulo === ingreseSuBusqueda);
  alert(busqueda);
 
-
-     if(busqueda != undefined){
-         alert("El articulo se encuentra en stock");
-         alert(busqueda);
-     }
-     else{
-         alert("El articulo ingresado no se encuentra en stock");
-     }
+    if(busqueda != undefined){
+        alert("El articulo se encuentra en stock");
+        alert(busqueda);
+    }
+    else{
+        alert("El articulo ingresado no se encuentra en stock");
+    }
 
   //FILTRO LA LIBRERIA CON PRECIOS MENORES QUE EL MONTO INGRESADO
  let filtroDeBusqueda=parseInt(prompt("Ingrese un monto que desee no superar por articulo"));
